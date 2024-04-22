82ec0a39d464caddcd98e7ea05b600a46b8137cd
porkhunkateryna <75451945+porkhunkateryna@users.noreply.github.com>
2024-04-22 10:56:34 +0300

    Delete l17/src/t33 directory

--- l17/src/t33/User.java
+++
@@ -1,8 +0,0 @@
-package t33;
-
-public interface User {
-    String getUserId();
-    void sendMessageAll(String message);
-    void sendMessage(String message, String userTo);
-    void receiveMessage(String message, String userFrom);
-}
